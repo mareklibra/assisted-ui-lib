a1eae4d742ea54a1ba0938662246f16ff1bd1318
Marek Libra <marek.libra@gmail.com>
2021-07-13 12:26:58 +0200

    Refactor directory structure
    From now on, we use following high-level subcomponents:
    - src/ocm
    - src/cim
    - src/common
    
    A consuming applicatoin should import either
    - index-ocm.ts or
    - index-cim.ts

---
+++ src/common/index.ts
@@ -0,0 +1,18 @@
+import './styles.css';
+import './hacks';
+
+export * from './components';
+export * from './api';
+export * from './types';
+export * from './config';
+export * from './features';
+
+/*
+
+export * as Config from './config';
+export * as Selectors from './selectors';
+export * as Store from './store';
+export * as Features from './features';
+export * as Reducers from './reducers';
+export * as Types from './types';
+*/
